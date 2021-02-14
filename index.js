2cd47eed7d83d1fee6e973b47ddac2f77fea94a3
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 02:36:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ let num2 = 2;
 
 let multiply = num1*num2;
 let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 12;
+let num4 = 8;
+let mod = num3%num4;
+let array = [1, 2, 3, 4, 20];
+let max = Math.max(array)
